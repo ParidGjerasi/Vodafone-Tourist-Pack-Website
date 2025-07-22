7c838b7ba5d00413d0e162efec6825883c0c3c5a
Parid <ridigjerasi@gmail.com>
2025-07-22 23:55:18 +0200

    Add Button component and berti image

---
+++ app/Button.jsx
@@ -0,0 +1,15 @@
+"use client";
+import { useState } from "react";
+
+export default function Button() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <button
+      onClick={() => setCount(count + 1)}
+      className="click-button"
+    >
+      Earned {count} GB!!!!!
+    </button>
+  );
+}
